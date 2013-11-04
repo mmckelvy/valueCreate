8722151cad465f13747bed998e8f58e52b180ed1
Mark McKelvy <mmckelvy@gmail.com>
2013-11-04 14:18:03 -0700

    minor updates

--- database/users.js
+++ database/users.js
@@ -1,8 +1,15 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 // Create the user schema.
-var userSchema = new Schema ({
+var companySchema = new Schema ({
 	name: String,
+	yr1rev: Number,
+	revgrowth: Number,
+	ebitdaPct: Number,
+	dNaPct: Number,
+	capexPct: Number,
+	cnwcPct: Number
+
 });
 // Create the User model with the user schema.
 var User = mongoose.model('User', userSchema);
