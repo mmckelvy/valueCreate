a166a4fb4303c56335bf7c2e9fd34d286b937693
RU-018 <mmckelvy@gmail.com>
2013-11-06 10:47:53 -0700

    first draft company input form

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -2,11 +2,11 @@ $(function () {
 	// Start with create a new company.
 	// On click of new company, send an ajax request, render a form via handlebars.
 	$(document).on('click', '#newCo', function (e) {
-		console.log('test');
 		var templateSource = $('#newCo-template').html();
 		var renderTemplate = Handlebars.compile(templateSource);
 		var rendered = renderTemplate({});
 		$('.maincontent').append(rendered);
+		$('#company-name').focus();
 		
 	});		
 });
