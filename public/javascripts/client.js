3f98be59fdf3e4da0a003734f354ec854f7ff241
RU-018 <mmckelvy@gmail.com>
2013-11-06 11:55:58 -0700

    first complete of add company interface

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -2,11 +2,15 @@ $(function () {
 	// Start with create a new company.
 	// On click of new company, send an ajax request, render a form via handlebars.
 	$(document).on('click', '#newCo', function (e) {
-		var templateSource = $('#newCo-template').html();
-		var renderTemplate = Handlebars.compile(templateSource);
-		var rendered = renderTemplate({});
-		$('.maincontent').append(rendered);
-		$('#company-name').focus();
-		
+		if ($('#add-company-container').length === 0) { 
+			var templateSource = $('#newCo-template').html();
+			var renderTemplate = Handlebars.compile(templateSource);
+			var rendered = renderTemplate({});
+			$('.maincontent').append(rendered);
+			setTimeout(function () {
+				$('#add-company-container').addClass('show');
+				$('#company-name').focus();
+			},0);
+		}
 	});		
 });
