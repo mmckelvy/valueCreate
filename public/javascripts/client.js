19201f4790c37190f376ce86971c9fa02573d0b2
RU-018 <mmckelvy@gmail.com>
2013-11-10 19:06:20 -0700

    started charting

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,35 +1,30 @@
 $(function () {
 	
 	// RENDERING
-	var renderInputForm = function () {
-		var templateSource = $('#newCo-template').html();
+	var renderElements = function (template) {
+		var templateSource = template.html();
 		var renderTemplate = Handlebars.compile(templateSource);
 		var rendered = renderTemplate({});
 		$('.maincontent').append(rendered);
 	};
 	
-	var showInputForm = function (displayElement, focusElement) {
+	var showElements = function (displayElement, focusElement) {
 		displayElement.addClass('show');
 		focusElement.focus();
 	};
-
 	// EVENTS
 	// On click of the 'new company' text, render a new company input form via Handlebars.
 	$('.introcontent').on('click', '#newCo', function (e) {
 		if ($('#add-company-container').length === 0) { 
-			renderInputForm();
-			// setTimeout(function () {
-			// 	$('#add-company-container').addClass('show');
-			// 	$('#company-name').focus();
-			// },0);
+			renderElements($('#newCo-template'));
 			setTimeout( function () {
-				showInputForm($('#add-company-container'), $('#first-input')); 
-				// $('#company-name').focus();
+				showElements($('#add-company-container'), $('#first-input')); 
 			}, 0 );
 		}
 	});		
 	
 	// On click of the submit form, send the form data to the server.
+    
 	$('.maincontent').on('submit', '#add-company-form', function (e) {
 		e.preventDefault();
 		$.post ('/newcompany', $('#add-company-form').serialize(), function (data) {
@@ -38,13 +33,72 @@ $(function () {
 			var results = data;
 			console.log(results);
 		});
-		// Clear and hide the form.
-		// Create a chart. [should be a function].
-		$('#add-company-container').addClass('animated flipOutX');
-		setTimeout( function () {
-			$('#add-company-container').empty();
-		}, 1000);
+
+		$('.maincontent').empty();
+		renderElements($('#chart-template'));
 	});
 
+	// CHARTING
+    $('#chart1-container').highcharts({
+        chart: {
+            type: 'column'
+        },
+        title: {
+            text: 'Stacked column chart'
+        },
+        xAxis: {
+            categories: ['Apples', 'Oranges', 'Pears', 'Grapes', 'Bananas']
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Total fruit consumption'
+            },
+            stackLabels: {
+                enabled: true,
+                style: {
+                    fontWeight: 'bold',
+                    color: (Highcharts.theme && Highcharts.theme.textColor) || 'gray'
+                }
+            }
+        },
+        legend: {
+            align: 'right',
+            x: -70,
+            verticalAlign: 'top',
+            y: 20,
+            floating: true,
+            backgroundColor: (Highcharts.theme && Highcharts.theme.legendBackgroundColorSolid) || 'white',
+            borderColor: '#CCC',
+            borderWidth: 1,
+            shadow: false
+        },
+        tooltip: {
+            formatter: function() {
+                return '<b>'+ this.x +'</b><br/>'+
+                    this.series.name +': '+ this.y +'<br/>'+
+                    'Total: '+ this.point.stackTotal;
+            }
+        },
+        plotOptions: {
+            column: {
+                stacking: 'normal',
+                dataLabels: {
+                    enabled: true,
+                    color: (Highcharts.theme && Highcharts.theme.dataLabelsColor) || 'white'
+                }
+            }
+        },
+        series: [{
+            name: 'John',
+            data: [5, 3, 4, 7, 2]
+        }, {
+            name: 'Jane',
+            data: [2, 2, 3, 2, 1]
+        }, {
+            name: 'Joe',
+            data: [3, 4, 4, 2, 5]
+        }]
+    });
 	
 });
